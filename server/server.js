fd9d4442ab1e8d7d6d73fe5eeb86f168e774318e
Semiha <semihakedir1@gmail.com>
2026-07-07 04:21:16 -0700

    Connect Express server to PostgreSQL database

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const pool = require('./config/db');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ message: 'Database connected!', time: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
